9bc70a0cfa92e087845bbfd6688301b0d43797a7
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2024-08-28 01:07:05 +0530

    reactExamplesInit

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function page() {
-  return <div>Landing</div>;
+  return <div>Home page</div>;
 }
 
 export default page;
---
+++ frontend/src/components/MyComponent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MyComponent() {
+  return <div>ExampleComponent</div>;
+}
+
+export default MyComponent;
